4e7276da94cb50f9cb68787e94b9a3d2f32adcc2
junblee <topguide82@gmail.com>
2019-06-05 15:31:29 +0900

    php

---
+++ re.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+    <body>
+        <h2> 전송결과 </h2>
+        <p> name : <strong><?php echo $_POST["name"]; ?> </strong></p>
+        <p> name : <strong><?php echo $_POST["phone"]; ?> </strong></p>
+    </body>
+</html>
