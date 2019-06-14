3f49aa8d06bb6e8ea2f01069dc2c98b298c6d569
junblee <topguide82@gmail.com>
2019-06-14 13:49:39 +0900

    js

---
+++ js/main2.js
@@ -0,0 +1,23 @@
+document.write("hello first time");
+
+var x;
+console.log(x);
+
+var j= 20, s="string"
+
+console.log(j);
+console.log(s);
+
+var f = 10;
+var g;
+g = f++;
+console.log(g);
+console.log(f);
+
+var t_x = 10;
+var t_y = "10";
+console.log(t_x == t_y);
+console.log(t_x === t_y);
+
+var z;
+console.log(z = t_x == t_y ? 10 : 11 );
