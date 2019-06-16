cbcd8c0c63672530073415283511394f16a69509
junblee <topguide82@gmail.com>
2019-06-16 22:50:44 +0900

    JS13

---
+++ JS 13 function/main.js
@@ -0,0 +1,11 @@
+// var test = document.getElementById("test"); // 결과값 가진 test변수
+
+// function mulitply(x,y){
+//     return x * y;
+// }
+
+var test = document.getElementById("test");
+
+function multiply(x,y) {
+    return x * y;
+}
