fa6b41b30df807a47f939e1d0a96b08f46725ce4
junblee <topguide82@gmail.com>
2019-06-17 00:05:02 +0900

    JS13

--- JS 13 function/main.js
+++ JS 13 function/main.js
@@ -1,11 +1,13 @@
-// var test = document.getElementById("test"); // 결과값 가진 test변수
+var test = document.getElementById("test"); // 결과값 가진 test변수
 
-// function mulitply(x,y){
-//     return x * y;
+// 일반적인 함수선언
+// function multiply(x,y) {
+//     return x,y;
 // }
 
-var test = document.getElementById("test");
-
-function multiply(x,y) {
-    return x * y;
+// 익명함수 선언. 변수에 함수를 할당
+var multiply = function(x,y) {
+    return x,y;
 }
+
+test.innerHTML = multiply(2,4);
